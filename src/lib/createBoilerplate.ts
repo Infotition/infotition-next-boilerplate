40fd88c0afa07e4e4d4065a98a24ec0ea2d62151
DevTobias <tobi.kaerst@gmx.de>
2021-02-09 17:43:23 +0100

    full boilerplate done

--- src/lib/createBoilerplate.ts
+++ src/lib/createBoilerplate.ts
@@ -2,9 +2,16 @@
 
 import fs from 'fs';
 import copyfiles from 'copyfiles';
+import path from 'path';
+
 import runComnmand from './runCommand';
 
+const main = require?.main?.filename || '';
+const publicDir = path.join(main, '../../public');
+
 async function createBoilerplate(dir: string) {
+  //* Create Folders and Files
+
   console.log(`---- creating project directory ----\n`);
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
@@ -14,40 +21,71 @@ async function createBoilerplate(dir: string) {
 
   console.log(`---- creating pages, api, public and styles folder ----\n`);
   fs.mkdirSync(`${dir}/src`);
+  fs.mkdirSync(`${dir}/src/components`);
   fs.mkdirSync(`${dir}/src/pages`);
   fs.mkdirSync(`${dir}/src/pages/api`);
   fs.mkdirSync(`${dir}/src/public`);
   fs.mkdirSync(`${dir}/src/styles`);
 
-  console.log(`---- creating index.tsc file ----\n`);
-  fs.writeFileSync(
-    `${dir}/src/pages/index.tsx`,
-    "import { NextPage } from 'next';\n\nconst Index: NextPage = () => (\n  <div>\n    <h1>Infotition Next.js Boilerplate 💡</h1>\n  </div>\n);\n\nexport default Index;"
-  );
-
-  console.log(`---- creating index.tsc file ----\n`);
+  console.log(`---- creating index.tsx file ----\n`);
   copyfiles(
-    ['../public/index.tsx', `${dir}/src/pages`],
+    [`${publicDir}/index.tsx`, `${dir}/src/pages`],
     { up: true },
     () => {}
   );
 
-  console.log(`---- creating _app.tsc file ----\n`);
-  copyfiles(['../public/_app.tsx', `${dir}/src/pages`], { up: true }, () => {});
+  console.log(`---- creating _app.tsx file ----\n`);
+  copyfiles(
+    [`${publicDir}/_app.tsx`, `${dir}/src/pages`],
+    { up: true },
+    () => {}
+  );
 
   console.log(`---- creating globals.scss file ----\n`);
   copyfiles(
-    ['../public/globals.scss', `${dir}/src/styles`],
+    [`${publicDir}/globals.scss`, `${dir}/src/styles`],
     { up: true },
     () => {}
   );
 
   console.log(`---- creating favicon.ico file ----\n`);
   copyfiles(
-    ['../public/favicon.ico', `${dir}/src/public`],
+    [`${publicDir}/favicon.ico`, `${dir}/src/public`],
     { up: true },
     () => {}
   );
+
+  console.log(`---- creating tsconfig.json file ----\n`);
+  copyfiles([`${publicDir}/tsconfig.json`, `${dir}`], { up: true }, () => {});
+
+  console.log(`---- creating .prettierrc file ----\n`);
+  copyfiles([`${publicDir}/.prettierrc`, `${dir}`], { up: true }, () => {});
+
+  console.log(`---- creating .prettierignore file ----\n`);
+  copyfiles([`${publicDir}/.prettierignore`, `${dir}`], { up: true }, () => {});
+
+  console.log(`---- creating .eslintrc file ----\n`);
+  copyfiles([`${publicDir}/.eslintrc`, `${dir}`], { up: true }, () => {});
+
+  console.log(`---- creating .eslintignore file ----\n`);
+  copyfiles([`${publicDir}/.eslintignore`, `${dir}`], { up: true }, () => {});
+
+  console.log(`---- creating .editorconfig file ----\n`);
+  copyfiles([`${publicDir}/.editorconfig`, `${dir}`], { up: true }, () => {});
+
+  console.log(`---- creating .stylelintrc.json file ----\n`);
+  copyfiles(
+    [`${publicDir}/.stylelintrc.json`, `${dir}`],
+    { up: true },
+    () => {}
+  );
+
+  console.log(`---- creating .gitignore file ----\n`);
+  // copyfiles([`${publicDir}/.gitignore`, `${dir}`], { up: true }, () => {});
+  const ignore = `# dependencies\n/node_modules\n/.pnp\n.pnp.js\n\n# testing\n/coverage\n\n# next.js\n/.next/\n/out/\n\n# production\n/build\n\n# misc\n.DS_Store\n*.pem\n\n# debug\nnpm-debug.log*\nyarn-debug.log*\nyarn-error.log*\n\n# local env files\n.env.local\n.env.development.local\n.env.test.local\n.env.production.local\n\n# vercel\n.vercel`;
+  fs.writeFileSync(`${dir}/.gitignore`, ignore, 'utf8');
+
+  //* Initializing npm project
 
   console.log(`---- create and edit package.json file ----\n`);
   await runComnmand(dir, 'npm init -y');
@@ -86,6 +124,8 @@ async function createBoilerplate(dir: string) {
     'utf8'
   );
 
+  //* Installing dependencies
+
   console.log(`---- installing next.js dependencies ----\n`);
   await runComnmand(dir, 'npm install react react-dom next');
 
@@ -94,9 +134,6 @@ async function createBoilerplate(dir: string) {
     dir,
     'npm install typescript @types/react @types/react-dom @types/node -D'
   );
-
-  console.log(`---- creating tsconfig.json file ----\n`);
-  fs.writeFileSync(`${dir}/tsconfig.json`, '', 'utf8');
 
   console.log(`---- installing linting dev dependencies ----\n`);
   await runComnmand(
@@ -116,26 +153,13 @@ async function createBoilerplate(dir: string) {
     'npm install stylelint stylelint-config-sass-guidelines -D'
   );
 
-  console.log(`---- creating .prettierrc file ----\n`);
-  copyfiles(['../public/.prettierrc', `${dir}`], { up: true }, () => {});
+  //* Initialize Project
 
-  console.log(`---- creating .prettierignore file ----\n`);
-  copyfiles(['../public/.prettierignore', `${dir}`], { up: true }, () => {});
+  console.log(`---- initialize git repository ----\n`);
+  await runComnmand(dir, 'git init');
 
-  console.log(`---- creating .gitignore file ----\n`);
-  copyfiles(['../public/.gitignore', `${dir}`], { up: true }, () => {});
-
-  console.log(`---- creating .eslintrc file ----\n`);
-  copyfiles(['../public/.eslintrc', `${dir}`], { up: true }, () => {});
-
-  console.log(`---- creating .eslintignore file ----\n`);
-  copyfiles(['../public/.eslintignore', `${dir}`], { up: true }, () => {});
-
-  console.log(`---- creating .editorconfig file ----\n`);
-  copyfiles(['../public/.editorconfig', `${dir}`], { up: true }, () => {});
-
-  console.log(`---- creating .stylelintrc.json file ----\n`);
-  copyfiles(['../public/.stylelintrc.json', `${dir}`], { up: true }, () => {});
+  console.log(`---- building next.js project ----\n`);
+  await runComnmand(dir, 'npm run build');
 }
 
 export default createBoilerplate;
